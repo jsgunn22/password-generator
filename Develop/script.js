a3efbe7002f1beef6e5865c8e7383d583c2b1926
jgunn <j.s.gunn22@gmail.com>
2023-06-28 11:29:20 -0700

    adds character options in arrays collected in object characters

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,14 @@
 // Assignment code here
+
+let characters = {
+  // prettier-ignore
+  charLowerCase: ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
+  // prettier-ignore
+  charUpperCase: ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"],
+  charNumber: ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"],
+  charSpecial: ["!", "@", "#", "$", "%", "&", "*", "?", "/", "~"],
+};
+
 function generatePassword() {
   let charCount = promp(
     "How many characters whould you like your password to contain"
